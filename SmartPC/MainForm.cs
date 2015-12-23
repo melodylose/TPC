3328f365080ee41b26c15f76439cfd8f32529a19
melodylose <jsdklose@hotmail.com>
2015-12-23 18:06:08 +0800

    設定產生Control屬性

--- SmartPC/MainForm.cs
+++ SmartPC/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,9 +13,66 @@ namespace SmartPC
 {
     public partial class MainForm : Form
     {
+        public ObservableCollection<WipParameterInfo> ParameterList { get; set; } = new ObservableCollection<WipParameterInfo>();
+
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeParameters();
+            ShowParameters();
+        }
+
+        private void InitializeParameters()
+        {
+            foreach (var index in Enumerable.Range(1, 8))
+            {
+                ParameterList.Add(new WipParameterInfo
+                {
+                    Sequential = index,
+                    ItemID = $"ItemID_{index}",
+                    ItemCaption = $"ItemCaption_{index}"
+                });
+            }
+        }
+
+        private void ShowParameters(int m_PageIndex = 1)
+        {
+            var list = ParameterList.Skip(4 * (m_PageIndex - 1)).Take(4).ToList();
+
+            for (int r = 0; r < tlp.RowCount; r++)
+            {
+                tlp.Controls.Add(InitializeLabel(list[r].Sequential.ToString()), 0, r);
+                tlp.Controls.Add(InitializeLabel(list[r].ItemID), 1, r);
+                tlp.Controls.Add(InitializeLabel(list[r].ItemCaption), 2, r);
+                tlp.Controls.Add(InitializeLabel(list[r].ItemCaption), 3, r);
+                tlp.Controls.Add(InitializeTextBox(), 4, r);
+            }
+        }
+
+        private TextBox InitializeTextBox()
+        {
+            return new TextBox
+            {
+                Dock = DockStyle.Fill,
+                Margin = new Padding(5),
+                Multiline = true
+            };
+        }
+
+        private Label InitializeLabel(string m_Text)
+        {
+            var lb = new Label
+            {
+                Text = m_Text,
+                BackColor = Color.FromArgb(224, 224, 224),
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Margin = new Padding(5)
+            };
+            
+            lb.Font = new Font("微軟正黑體", 12f);
+            return lb;
         }
     }
 }
---
+++ SmartPC/WipParameterInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPC
+{
+    public class WipParameterInfo
+    {        
+        public int Sequential { get; set; }        
+        public string ItemID { get; set; }
+        public string ItemCaption { get; set; }
+        public string ParameterStandard { get; set; }
+        public string ParameterUpperLimit { get; set; }
+        public string ParameterLowerLimit { get; set; }
+        public string ParameterValue { get; set; }
+        public bool IsNumeric { get; set; } = false;
+        public Decimal? ParameterLowerValue { get; set; } = 0;
+        public Decimal? ParameterUpperValue { get; set; } = 0;
+    }
+}
